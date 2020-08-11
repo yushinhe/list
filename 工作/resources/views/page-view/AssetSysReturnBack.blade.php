84682fd6116824b0254b63979b018343ab329616
unknown <robin124708476@gmail.com>
2020-08-11 10:10:46 +0800

    先不放刪除

--- 工作/resources/views/page-view/AssetSysReturnBack.blade.php
+++ 工作/resources/views/page-view/AssetSysReturnBack.blade.php
@@ -6,14 +6,14 @@
           <p>歸還明細</p>
 
           @foreach ($borrow as $b)
-          <div class="d-flex">
+          {{-- <div class="d-flex"> --}}
           <a href="/asset-sys-returnback-2/{{$b->id}}"
             class="list-group-item list-group-item-action">
             {{ '品項名稱:' . $b->object . '   ' . '借用時間:' . $b->borrowtime .  '  入庫時間:' . $b->backtime }}</a> 
-            <form action="" method="POST">
+            {{-- <form action="" method="POST">
               {{ csrf_field() }}
               {{method_field('DELETE')}}
              <input type="submit" value="刪除"></div>
-          </form>
+          </form> --}}
     @endforeach
           @endsection  

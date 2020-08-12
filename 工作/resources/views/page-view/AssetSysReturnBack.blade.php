739de2def464b0f0fa48f8ef65a1e83e9b224659
robin <robin124708476@gmail.com>
2020-08-12 09:43:17 +0800

    改頁面

--- 工作/resources/views/page-view/AssetSysReturnBack.blade.php
+++ 工作/resources/views/page-view/AssetSysReturnBack.blade.php
@@ -13,7 +13,7 @@
           {{-- <div class="d-flex"> --}}
           <a href="/asset-sys-returnback-2/{{$b->id}}"
             class="list-group-item list-group-item-action">
-            {{ '品項名稱:' . $b->object . '   ' . '借用時間:' . $b->borrowtime .  '  入庫時間:' . $b->backtime }}</a> 
+            {{ '品項名稱:' . $b->object . ' 借用人:' . $b->person. '  ' . '歸還時間:' . $b->senddated .  '  入庫時間:' . $b->backtime }}</a> 
             {{-- <form action="" method="POST"> --}}
               {{-- {{ csrf_field() }}
               {{method_field('DELETE')}}

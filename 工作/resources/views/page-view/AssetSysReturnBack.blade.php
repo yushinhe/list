07188d97ee7ac2f3576d331221848f1d867fa0c7
yushinhe <enter>
2020-09-09 09:04:46 +0800

    時間標示

--- 工作/resources/views/page-view/AssetSysReturnBack.blade.php
+++ 工作/resources/views/page-view/AssetSysReturnBack.blade.php
@@ -11,8 +11,16 @@
         <p>歸還明細</p>
 
         @foreach ($borrow as $b)
-            <a href="{{ route('returnback.show', $b->id) }}" class="list-group-item list-group-item-action">
-                {{ ' 借用人:' . $b->person . '  ' . '歸還時間:' . $b->senddated . '  入庫時間:' . $b->backtime }}</a>
+        @if($b->senddated > $b->sendtime)
+        <a href="{{ route('returnback.show', $b->id) }}" class="list-group-item list-group-item-action text-danger">
+           {{ ' 借用人:' . $b->person . '  ' . '歸還時間:' . $b->senddated . '  入庫時間:' . $b->backtime }}</a>
+           @elseif($b->senddated == "" && $b->senddated <now() )
+            <a href="{{ route('returnback.show', $b->id) }}" class="list-group-item list-group-item-action text-warning">
+               {{ ' 借用人:' . $b->person . '  ' . '歸還時間:' . $b->senddated . '  入庫時間:' . $b->backtime }}</a>
+               @else
+           <a href="{{ route('returnback.show', $b->id) }}" class="list-group-item list-group-item-action">
+               {{ ' 借用人:' . $b->person . '  ' . '歸還時間:' . $b->senddated . '  入庫時間:' . $b->backtime }}</a>
+               @endif
         @endforeach
     @endif
 
